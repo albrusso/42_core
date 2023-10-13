abd707fad8eca1df06e0d311c38935a999ab0b1c
42albrusso <42albrusso@gmail.com>
2023-10-13 16:25:07 +0200

    philosopher

--- philosopher/routine.c
+++ philosopher/routine.c
@@ -5,9 +5,32 @@ bool  philo_die(t_philo *philo)
 {
       pthread_mutex_lock(&philo->args->root);
       if ((philo->meal_count!= 0) && (get_time() - philo->time_last_meal >= philo->args->time_to_die))
-            return (false);
+            return (true);
       pthread_mutex_unlock(&philo->args->root);
-      return (true);
+      return (false);
+}
+
+bool  all_eat(t_philo *philo)
+{
+      int   i;
+      int   j;
+
+      i = 0;
+      j = 0;
+      if (philo->args->nbr_of_time == -1)
+            return (false);
+      else
+      {
+            while (i < philo->args->nbr_of_philo)
+            {
+                  if (philo->meal_count == philo->args->nbr_of_time)
+                        j++;
+                  i++;
+            }
+            if (j == philo->args->nbr_of_philo)
+                  return (true);
+            return (false);
+      }
 }
 
 void  *routine(void *_philo)
@@ -17,8 +40,8 @@ void  *routine(void *_philo)
       philo = (t_philo *)_philo;
       while (1)
       {
-            if (philo_die(philo) == false)
-                  exit(1);
+            if (philo_die(philo) == true || all_eat(philo) == true)
+                 return (NULL);
             p_eat(philo);
             p_sleep(philo);
             supervisor(P_THINK, philo);
